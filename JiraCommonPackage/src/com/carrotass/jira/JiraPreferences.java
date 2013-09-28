cfbfaec030b5a48cdd4810b9bdc476eb328ce6e7
de1mos <de1m0s242@gmail.com>
2013-09-28 13:44:19 +0800

    Issue #1

--- JiraCommonPackage/src/com/carrotass/jira/JiraPreferences.java
+++ JiraCommonPackage/src/com/carrotass/jira/JiraPreferences.java
@@ -1,12 +1,23 @@
-package com.carrotass.preferences;
+package com.carrotass.jira;
 
-public class JiraLoginPreferences {
+import com.carrotass.jira.Jira.JiraType;
+
+public class JiraPreferences {
+	
 	java.util.prefs.Preferences preferences = 
 			java.util.prefs.Preferences.userNodeForPackage(getClass());
 	
 	public void setCredentials(String username, String password) {
 		preferences.put("jira_username", username);
 		preferences.put("jira_password", password);
+	}
+	
+	public void setJiraPath(JiraType type, String path) {
+		preferences.put(type.toString(), path);
+	}
+	
+	public String getJiraPath(JiraType type) {
+		return preferences.get(type.toString(), null);
 	}
 	
 	public String getUsername() {
